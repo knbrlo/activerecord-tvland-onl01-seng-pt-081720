b946810299548f8b5feef69d82275c335091ff50
knbrlo <kenbrlow@gmail.com>
2020-12-04 19:39:50 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full_name_combined = "#{self.first_name} #{self.last_name}"
+    full_name_combined
+  end
+
+  def list_roles
+    self.characters.collect do |character|
+        "#{character.name} - #{character.show.name}"
+    end
+  end
 end
